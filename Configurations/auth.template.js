92d584366b4fa463aa618399e790a2f6be8cd560
ccaa12345671-arch <ccaa12345671@gmail.com>
2026-09-03 15:10:59 +0200

    Update Discord client credentials
    Updated Discord client credentials in auth.template.js

--- Configurations/auth.template.js
+++ Configurations/auth.template.js
@@ -1,8 +1,8 @@
 module.exports = {
 	discord: {
-		clientID: "",
-		clientSecret: "",
-		clientToken: "",
+		clientID: "1487791192761565204",
+		clientSecret: "1nc341dllmFlpRmICxC_ojXvqYU8iNk-",
+		clientToken: "MTQ4Nzc5MTE5Mjc2MTU2NTIwNA.GYkm_r.gyuCjuW4OMwM7iY4sojEi4ckFab-mZ82vSD5rg",
 	},
 	tokens: {
 		discordBotsOrg: "",
